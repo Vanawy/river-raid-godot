4e2936a873d79be6323e1057a77de09d8042eb81
Ilya Sakharchuk <saharchuk999@gmail.com>
2023-12-18 13:47:18 +0300

    jjjj

--- Jet.gd
+++ Jet.gd
@@ -35,7 +35,7 @@ func _physics_process(delta):
 	var move = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	velocity.x = lerpf(velocity.x, H_SPEED * move.x, delta * 3)
-	velocity.y = -BASE_V_SPEED + move.y * V_SPEED_CHANGE
+	velocity.y = lerpf(velocity.y, -BASE_V_SPEED + move.y * V_SPEED_CHANGE, delta * 5)
 	smoke_emitter.amount_ratio = 1 + move.y * 0.3
 	
 	move_and_slide()
